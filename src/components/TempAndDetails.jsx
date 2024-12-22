37282e75609a2e0851fa9a7bae934793c64183e4
idajoh <bp17idajoh@gmail.com>
2024-12-22 02:32:03 +0100

    TempAndDetails - hardcoding for small icon an dinfo display

--- src/components/TempAndDetails.jsx
+++ src/components/TempAndDetails.jsx
@@ -8,6 +8,56 @@ import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 
 const TempAndDetails = () => {
+
+    const verticalDetails = [
+        {
+            id: 1,
+            Icon: FaThermometerEmpty,
+            title: "Real Feel",
+            value: "22°",
+        },
+        {
+            id: 2,
+            Icon: BiSolidDropletHalf,
+            title: "Humidity",
+            value: "36%",
+        },
+        {
+            id: 3,
+            Icon: FiWind,
+            title: "Wind",
+            value: "11 km/h",
+        },
+    ];
+
+    const horizontalDetails = [
+        {
+            id: 1,
+            Icon: GiSunrise,
+            title: "Sunrise",
+            value: "05:33 AM",
+        },
+        {
+            id: 2,
+            Icon: GiSunset,
+            title: "Sunset",
+            value: "08:33 PM",
+        },
+        {
+            id: 3,
+            Icon: MdKeyboardArrowUp,
+            title: "High",
+            value: "37°",
+        },
+        {
+            id: 4,
+            Icon: MdKeyboardArrowDown,
+            title: "Low",
+            value: "7°",
+        },
+    ];
+    
+
   return (
     <div>
         <div className="flex items-center justify-center py-6 text-xl text-cyan-300">  
@@ -15,7 +65,23 @@ const TempAndDetails = () => {
         </div>
 
         <div className="flex flex-row items-center justify-between py-3">
-            <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather sun Icon" />
+            <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather sun Icon"  className="w-20 "/>
+
+            <p className="text-5xl"> 30° </p>
+
+            <div className="flex flex-col space-y-3 items-start">
+
+                {
+                    verticalDetails.map (({id, Icon, title, value}) => (
+                        <div key={id} className="flex font-light text-sm items-center justify-center">
+                        <Icon  size={18} className="mr-1"/>
+                        {`${title}:`} 
+                         <span className="font-medium ml-1"> {value} </span>
+                    </div>
+                    ))
+                }
+
+            </div>
         </div>
 
     </div>
